52d2cef3da6a9a8db0708e637da4b1e7e41ea4da
devra <medev.rajdon1@gmail.com>
2024-01-01 15:14:49 +0545

    login and drawer

---
+++ app/src/main/java/com/example/deliveryapp/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.example.deliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText username, password;
+    Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        loginBtn = findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isInputValid()){
+                    String uName = username.getText().toString();
+                    String pass = password.getText().toString();
+                    loginPerformed(uName,pass);
+                }else{
+                    Toast.makeText(LoginActivity.this,"Fill all the field", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private boolean isInputValid(){
+        return !username.getText().toString().trim().isEmpty() && !password.getText().toString().trim().isEmpty();
+    }
+    private void loginPerformed(String username, String password){
+        if(username.equals("admin") && password.equals("123456")){
+            Toast.makeText(this,"Successfully Logged in", Toast.LENGTH_SHORT ).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Password or username incorrect", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
